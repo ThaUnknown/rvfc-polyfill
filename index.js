21c1caedc09deb9994174078600487fe0f6bcf38
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-06-16 22:31:45 +0200

    fix: incorrect mediatime

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (!('requestVideoFrameCallback' in HTMLVideoElement.prototype) && 'getVideoPla
           expectedDisplayTime: now + timediff,
           width: this.videoWidth,
           height: this.videoHeight,
-          mediaTime: Math.max(0, this.currentTime || 0) + timediff,
+          mediaTime: Math.max(0, this.currentTime || 0) + timediff / 1000,
           presentedFrames,
           processingDuration
         })
